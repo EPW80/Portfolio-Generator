4d197843373fec8ee3df6662f52e8340d075bdd3
Erik Williams <epfnw1@gmail.com>
2022-04-25 18:06:58 -0700

    merge develop into main

--- app.js
+++ app.js
@@ -5,13 +5,3 @@ const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-<<<<<<< HEAD
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-=======
->>>>>>> develop
